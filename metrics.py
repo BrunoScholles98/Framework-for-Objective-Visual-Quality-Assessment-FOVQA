c232a555329f84887b62c2fa596665aa6436804f
Bruno <brunoscholles@outlook.com>
2023-02-26 00:24:12 -0300

    Added Comments in the Metrics Code

--- metrics.py
+++ metrics.py
@@ -15,21 +15,26 @@ from tensorflow import keras
 #-----------------------------------------Skvideo Full Reference--------------------------------------------
 #-----------------------------------------------------------------------------------------------------------
 def measureSkvideoMetric(metric_func, metric_name, videoRef, videoDist, h, w, refpath, distpath):
+    # Create strings containing paths to reference and distorted files
     reference = f"{refpath}{videoRef}{'.yuv'}"
     dist = f"{distpath}{videoDist}{'.yuv'}"
 
+    # Print information about the metric and videos being compared
     print(f"Metric: {metric_name}")
     print(f"Reference: {videoRef}")
     print(f"Distortion: {videoDist}")
 
+    # Load reference and distorted videos into memory as numpy arrays using scikit-video library
     ref = skvideo.io.vread(reference, h, w, as_grey=True)
     dis = skvideo.io.vread(dist, h, w, as_grey=True)
 
+    # Calculate video quality metric score using the function provided as argument and the average score
     scores = metric_func(ref, dis)
     avg_score = np.mean(scores)
 
     print(f"AvScore: {avg_score}")
     print('\n\n')
+
     return avg_score
 
 def measureSSIM(videoRef, videoDist, h, w, refpath, distpath, vmaf_model):
@@ -65,75 +70,99 @@ def measureNIQE(videoDist, h, w, distpath, vmaf_model):
 #-----------------------------------------VMAF--------------------------------------------
 #-----------------------------------------------------------------------------------------
 def measureVMAF(videoRef, videoDist, h, w, refpath, distpath, vmaf_model):
-    command1 = f"{'ffmpeg -video_size '}{w}{'x'}{h}{' -i '}{distpath}"
-    command2 = f"{' -video_size '}{w}{'x'}{h}{' -i '}{refpath}"
-    command3 = ' -lavfi libvmaf="model_path=' + vmaf_model + '" -f null -'
-    command = f"{command1}{videoDist}{'.yuv'}{command2}{videoRef}{'.yuv'}{command3}"
+    # Construct FFmpeg command to calculate VMAF score for given videos
+    command = f"ffmpeg -video_size {w}x{h} -i {distpath}{videoDist}.yuv -video_size {w}x{h} -i {refpath}{videoRef}.yuv -lavfi libvmaf=model_path={vmaf_model} -f null -"
 
-    print(command)
-
+    # Print information about the metric and videos being compared
     print('Metric: VMAF')
     print(f"{'Reference: '}{videoRef}")
     print(f"{'Distortion: '}{videoDist}")
-    
+
+    # Execute FFmpeg command and capture the output
     output = sp.getoutput(command)
-    print(output)
+
+    # Split the output to extract the VMAF score
     splitter = 'VMAF score: '
     score = output.split(splitter)[1]
 
     print(f"{'VMAF Score: '}{score}")
     print('\n\n')
+
     return score
 
 #-------------------------------------------------------------------------------------------------
 #--------------------------------------------PyMetrikz--------------------------------------------
 #-------------------------------------------------------------------------------------------------
 def measurePyMetrikz(videoRef, videoDist, h, w, refpath, distpath, vmaf_model, metric_func):
-
+    # Print metric name and video names
     print(f"{'Metric: '}{metric_func.__name__.upper()}")
     print(f"{'Reference: '}{videoRef}")
     print(f"{'Distortion: '}{videoDist}")
     
+    # Create an empty list to hold the metric results for each frame
     metricResults = []
+
+    # Set the path for the folder to store the frames
     path = './frames'
 
+    # Convert the reference and distorted videos to AVI format
     tools.convertionToAVI(videoRef, h, w, refpath)
     tools.convertionToAVI(videoDist, h, w, distpath)
 
+    # Set the path for the reference and distorted videos in AVI format
     stringPathRef = f"{'./videosAVI/'}{videoRef}{'.avi'}"
     stringPathDist = f"{'./videosAVI/'}{videoDist}{'.avi'}"
 
+    # Create OpenCV video capture objects for the reference and distorted videos
     ref = cv2.VideoCapture(stringPathRef)
     dist = cv2.VideoCapture(stringPathDist)
 
+    # Read the first frames from the reference and distorted videos
     successRef,framesRef = ref.read()
     successDist,framesDist = dist.read()
 
+    # Set counters for the frames in the reference and distorted videos, and an index counter
     countRef = 0
     countDist = 0
     i = 0
 
+    # Loop through the frames in the reference and distorted videos
     while successRef & successDist: 
+
+        # Write the current frames to the folder for the frames
         cv2.imwrite(os.path.join(path ,'frameRef%d.png') % countRef, framesRef)    
         successRef,framesRef = ref.read()
         cv2.imwrite(os.path.join(path ,'frameDist%d.png') % countDist, framesDist)      
         successDist,framesDist = dist.read()
+
+        # Set the paths for the current frames in the folder for the frames
         pathFrameRef = f"{'./frames/frameRef'}{i}{'.png'}"
         pathDistRef = f"{'./frames/frameDist'}{i}{'.png'}"
+
+        # Read the current frames from the folder for the frames using matplotlib image
         refFrameMeasure = mpimg.imread(pathFrameRef)
         distFrameMeasure = mpimg.imread(pathDistRef)
+
+        # Compute the metric value for the current frames using the specified metric function
         value = metric_func(refFrameMeasure, distFrameMeasure) 
+
+        # Append the metric value to the list of metric results
         metricResults.append(value)    
+
+        # Increment the counters for the frames in the reference and distorted videos, and the index counter
         countRef += 1
         countDist += 1
         i += 1
 
+    # Clean up the folder for the frames
     tools.cleanFrameFolder()
 
+    # Compute the average metric value across all frames
     score = np.mean(metricResults)
-    print(f"{metric_func.__name__.upper()} Score: {score}")
-    print('\n\n')
     
+    print(f"{metric_func.__name__.upper()} Score: {score}")
+    print('\n\n')    
+
     return score
 
 def measureRMSE(videoRef, videoDist, h, w, refpath, distpath, vmaf_model):
--- metrics_nd.py
+++ metrics_nd.py
@@ -14,22 +14,28 @@ from tensorflow import keras
 #-----------------------------------------------------------------------------------------------------------
 #-----------------------------------------Skvideo Full Reference--------------------------------------------
 #-----------------------------------------------------------------------------------------------------------
-def measureSkvideoMetric(metric_func, metric_name, videoRef, videoDist, formato, refpath, distpath, vmaf_model):
+def measureSkvideoMetric(metric_func, metric_name, videoRef, videoDist, formato, refpath, distpath, vmaf_model):    
+    # Defining reference and distorted video file paths
     reference = f"{refpath}{videoRef}{'.'}{formato}"
     dist = f"{distpath}{videoDist}{'.'}{formato}"
-
+    
+    # Printing metric name, reference video file name, and distorted video file name
     print(f"Metric: {metric_name}")
     print(f"Reference: {videoRef}")
     print(f"Distortion: {videoDist}")
-
+    
+    # Reading reference and distorted videos using scikit-video
     ref = skvideo.io.vread(reference, as_grey=True)
     dis = skvideo.io.vread(dist, as_grey=True)
-
-    scores = metric_func(ref, dis)
+    
+    # Calculating scores using the given metric function and average score from the list of scores
+    scores = metric_func(ref, dis)  
     avg_score = np.mean(scores)
-
+    
+    # Printing average score and adding newline characters for readability
     print(f"AvScore: {avg_score}")
-    print('\n\n')
+    print('\n\n')    
+
     return avg_score
 
 def measureSSIM(videoRef, videoDist, formato, refpath, distpath, vmaf_model):
@@ -65,71 +71,94 @@ def measureNIQE(videoDist, formato, distpath, vmaf_model):
 #-----------------------------------------VMAF--------------------------------------------
 #-----------------------------------------------------------------------------------------
 def measureVMAF(videoRef, videoDist, formato, refpath, distpath, vmaf_model):
-    command1 = f"{'ffmpeg -i '}{distpath}"
-    distVideo = videoDist
-    command2 = f"{' -i '}{refpath}"
-    refVideo = videoRef
-    command3 = ' -lavfi libvmaf="model_path=' + vmaf_model + '" -f null -'
-    command = f"{command1}{distVideo}{'.'}{formato}{command2}{refVideo}{'.'}{formato}{command3}"
-
+    # Constructing the ffmpeg command to calculate VMAF score
+    command = f"ffmpeg -i {distpath}{videoDist}.{formato} -i {refpath}{videoRef}.{formato} -lavfi libvmaf='model_path={vmaf_model}' -f null -"
+    
+    # Printing metric name, reference video file name, and distorted video file name
     print('Metric: VMAF')
     print(f"{'Reference: '}{videoRef}")
     print(f"{'Distortion: '}{videoDist}")
     
+    # Running the ffmpeg command using subprocess module
     output = sp.getoutput(command)
     print(output)
+    
+    # Extracting the VMAF score from the output using a splitter string
     splitter = 'VMAF score: '
-    score = output.split(splitter)[1]
+    score = output.split(splitter)[1]    
 
     print(f"{'VMAF Score: '}{score}")
-    print('\n\n')
+    print('\n\n')    
+    
     return score
 
 #-------------------------------------------------------------------------------------------------
 #--------------------------------------------PyMetrikz--------------------------------------------
 #-------------------------------------------------------------------------------------------------
 def measurePyMetrikz(videoRef, videoDist, formato, refpath, distpath, vmaf_model, metric_func):
-
+    # Printing metric name, reference video file name, and distorted video file name
     print(f"{'Metric: '}{metric_func.__name__.upper()}")
     print(f"{'Reference: '}{videoRef}")
     print(f"{'Distortion: '}{videoDist}")
     
+    # Initializing an empty list to store metric results for each frame
     metricResults = []
+    
+    # Setting the path to save the frames temporarily
     path = './frames'
-
+    
+    # Constructing the file paths for reference and distorted videos
     stringPathRef = f"{refpath}{videoRef}{'.'}{formato}"
     stringPathDist = f"{distpath}{videoDist}{'.'}{formato}"
-
+    
+    # Opening the reference and distorted videos using OpenCV
     ref = cv2.VideoCapture(stringPathRef)
     dist = cv2.VideoCapture(stringPathDist)
-
+    
+    # Reading the first frame from reference and distorted videos
     successRef,framesRef = ref.read()
     successDist,framesDist = dist.read()
-
+    
+    # Initializing counters for reference and distorted frames
     countRef = 0
     countDist = 0
     i = 0
-
+    
+    # Looping over all frames in the reference and distorted videos
     while successRef & successDist: 
+        
+        # Saving the current reference and distorted frames as images
         cv2.imwrite(os.path.join(path ,'frameRef%d.png') % countRef, framesRef)    
         successRef,framesRef = ref.read()
         cv2.imwrite(os.path.join(path ,'frameDist%d.png') % countDist, framesDist)      
         successDist,framesDist = dist.read()
+        
+        # Setting the file paths for the current reference and distorted frames
         pathFrameRef = f"{'./frames/frameRef'}{i}{'.png'}"
         pathDistRef = f"{'./frames/frameDist'}{i}{'.png'}"
+        
+        # Reading the current reference and distorted frames using matplotlib
         refFrameMeasure = mpimg.imread(pathFrameRef)
         distFrameMeasure = mpimg.imread(pathDistRef)
+        
+        # Calculating the metric value for the current reference and distorted frames
         value = metric_func(refFrameMeasure, distFrameMeasure) 
+        
+        # Storing the metric value in the list of metric results
         metricResults.append(value)    
+        
+        # Incrementing the counters for reference and distorted frames
         countRef += 1
         countDist += 1
         i += 1
-
+    
+    # Cleaning up the temporary folder used for storing frames
     tools.cleanFrameFolder()
-
+    
+    # Calculating the average metric value over all frames and printing it
     score = np.mean(metricResults)
     print(f"{metric_func.__name__.upper()} Score: {score}")
-    print('\n\n')
+    print('\n\n')    
     
     return score
 
